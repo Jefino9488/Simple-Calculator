7c93f0b6a968c5f0b0f8de074ac084dc0dfe8819
Jefino 1537 <jefinojacob1537@gmail.com>
2023-11-24 17:04:35 +0530

    Calculator

--- SimpleCalculator.py
+++ SimpleCalculator.py
@@ -30,60 +30,50 @@ class CalculatorApp:
         self.result_var = tk.StringVar()
         self.create_widgets()
 
-        # Bind the Enter key to perform the calculation
-        self.master.bind('<Return>', lambda event: self.on_button_click('='))
-        # Bind the Backspace key to the backspace function
-        self.master.bind('<BackSpace>', lambda event: self.on_backspace())
+        self.master.bind('<Return>', lambda event: self.on_button_click('='))  # bind '='
+        self.master.bind('<BackSpace>', lambda event: self.on_backspace())  # bind 'backspace'
 
-    def validate_entry(self, char):
-        # Allow only digits, '.', '+', '-', '*', '/'
+    @staticmethod
+    def validate_entry(self, char): # validate entry
         return char.isdigit() or char in ['.', '+', '-', '*', '/']
 
     def create_widgets(self):
-        # Entry widget to display and edit the expression
         entry = tk.Entry(self.master, textvariable=self.result_var, font=('Arial', 16), bd=5, insertwidth=2, width=14,
-                         justify='right', validate='key', validatecommand=(self.master.register(self.validate_entry), '%S'))
+                         justify='right', validate='key',
+                         validatecommand=(self.master.register(self.validate_entry), '%S'))
         entry.grid(row=1, column=0, columnspan=3)
 
-        # Buttons for digits and operations
         buttons = [
-            ('<-', 1, 3),
+            ('←', 1, 3),
             ('7', 2, 0), ('8', 2, 1), ('9', 2, 2), ('/', 2, 3),
             ('4', 3, 0), ('5', 3, 1), ('6', 3, 2), ('*', 3, 3),
             ('1', 4, 0), ('2', 4, 1), ('3', 4, 2), ('-', 4, 3),
             ('0', 5, 0), ('C', 5, 1), ('=', 5, 2), ('+', 5, 3),
         ]
 
-        # Create and place buttons
         for (text, row, col) in buttons:
             button = tk.Button(self.master, text=text, font=('Arial', 14), height=2, width=4,
                                command=lambda t=text: self.on_button_click(t) if t != '<-' else self.on_backspace())
             button.grid(row=row, column=col, padx=5, pady=5)
 
-    def on_button_click(self, button_text):
+    def on_button_click(self, button_text):  # on click button
         current_text = self.result_var.get()
 
         if button_text == 'C':
-            # Clear the entry
             self.result_var.set('')
         elif button_text == '=':
             try:
-                # Evaluate the expression and set the result
                 result = eval(current_text)
                 self.result_var.set(result)
-            except Exception as e:
-                # Handle invalid expressions
+            except Exception:
                 self.result_var.set('Error')
-        elif button_text == '<-':
-            # Backspace functionality
+        elif button_text == '←':
             self.on_backspace()
         else:
-            # Append the button text to the current expression
             self.result_var.set(current_text + button_text)
 
     def on_backspace(self):
-        current_text = self.result_var.get()
-        # Remove the last character from the entry
+        current_text = self.result_var.get()  # removes last character
         self.result_var.set(current_text[:-1])
 
 
